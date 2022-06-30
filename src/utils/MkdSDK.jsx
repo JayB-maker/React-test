3f7480df489a765b31fb9d05d8c483f265ed307c
manaknight <ryan@manaknight.com>
2022-06-30 21:41:03 +0000

    Update 'src/utils/MkdSDK.jsx'

--- src/utils/MkdSDK.jsx
+++ src/utils/MkdSDK.jsx
@@ -9,6 +9,10 @@ export default function MkdSDK() {
   const raw = this._project_id + ":" + this._secret;
   let base64Encode = btoa(raw);
 
+  this.setTable = function (table) {
+    this._table = table;
+  };
+  
   this.login = async function (email, password, role) {
     //TODO
   };
@@ -23,6 +27,64 @@ export default function MkdSDK() {
   this.baseUrl = function () {
     return this._baseurl;
   };
+  
+  this.callRestAPI = async function (payload, method) {
+    const header = {
+      "Content-Type": "application/json",
+      "x-project": base64Encode,
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    };
+
+    switch (method) {
+      case "GET":
+        const getResult = await fetch(
+          this._baseurl + `/v1/api/rest/${this._table}/GET`,
+          {
+            method: "post",
+            headers: header,
+            body: JSON.stringify(payload),
+          }
+        );
+        const jsonGet = await getResult.json();
+
+        if (getResult.status === 401) {
+          throw new Error(jsonGet.message);
+        }
+
+        if (getResult.status === 403) {
+          throw new Error(jsonGet.message);
+        }
+        return jsonGet;
+      
+      case "PAGINATE":
+        if (!payload.page) {
+          payload.page = 1;
+        }
+        if (!payload.limit) {
+          payload.limit = 10;
+        }
+        const paginateResult = await fetch(
+          this._baseurl + `/v1/api/rest/${this._table}/${method}`,
+          {
+            method: "post",
+            headers: header,
+            body: JSON.stringify(payload),
+          }
+        );
+        const jsonPaginate = await paginateResult.json();
+
+        if (paginateResult.status === 401) {
+          throw new Error(jsonPaginate.message);
+        }
+
+        if (paginateResult.status === 403) {
+          throw new Error(jsonPaginate.message);
+        }
+        return jsonPaginate;
+      default:
+        break;
+    }
+  };  
 
   this.check = async function (role) {
     //TODO
