bd0ee5247dfc26b669671cacdfdde0c4eb8fd947
JayB <ajiboyeabiodun001@gmail.com>
2022-08-25 08:52:29 +0200

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      text_gray: '#696969',
+      secondary: '#111111',
+      accent: '#9BFF00',
+      text_white: '#FFFFFF',
+      text_green: `#DBFD51`,
+      text_black: `#050505`,
+      card_gray: `#1d1d1d`,
+    }
   },
   plugins: [],
 }
+
