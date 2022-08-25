051af1c2e8b201e03cc9fbdd9f5f31982b0ce2ad
JayB <ajiboyeabiodun001@gmail.com>
2022-08-25 11:59:39 +0200

    new commit

--- src/pages/AdminDashboardPage.jsx
+++ src/pages/AdminDashboardPage.jsx
@@ -1,38 +1,80 @@
 import React from "react";
 import arrow from "./assets/arrow.png";
 import avatar from "./assets/avatar.png";
-import arrowup from "./assets/arrow-up.png";
+import arrow_up from "./assets/arrow-up.png";
+import MkdSDK from "../utils/MkdSDK";
 
 const AdminDashboardPage = () => {
-  const [data, setData] = React.useState({});
+  const [pageNumber, setPageNumber] = React.useState(1);
+  const [movieList, setMovieList] = React.useState([]);
 
-  const getMovieData = async () => {
-    await fetch(`https://reacttask.mkdlabs.com/v1/api/rest/video/PAGINATE`, {
-      method: "post",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("token"),
-      },
-      body: JSON.stringify(data),
-    }).then((response) => setData(response));
+  const getMovies = () => {
+    const sdk = new MkdSDK();
+    sdk
+      .callRestAPI(
+        {
+          payload: {},
+        },
+        "PAGINATE"
+      )
+      .then((response) => {
+        setMovieList(response.list);
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const nextPage = () => {
+    console.log("go");
+    pageNumber > 9 ? setPageNumber(11) : setPageNumber(pageNumber + 1);
+    const sdk = new MkdSDK();
+    sdk
+      .callRestAPI(
+        {
+          payload: {},
+          page: pageNumber,
+        },
+        "PAGINATE"
+      )
+      .then((response) => {
+        setMovieList(response.list);
+      })
+      .catch((err) => {});
+  };
+
+  const prevPage = () => {
+    console.log("go");
+    pageNumber > 1 ? setPageNumber(pageNumber - 1) : setPageNumber(1);
+    const sdk = new MkdSDK();
+    sdk
+      .callRestAPI(
+        {
+          payload: {},
+          page: pageNumber,
+        },
+        "PAGINATE"
+      )
+      .then((response) => {
+        setMovieList(response.list);
+      })
+      .catch((err) => {});
   };
 
   React.useEffect(() => {
-    getMovieData();
+    getMovies();
   }, []);
 
   return (
     <>
-      <div className="w-full fixed top-0 left-0 bg-secondary flex justify-center text-7xl h-screen text-gray-700 h-100vh position-fixed py-5 px-20">
-        <div className="container">
+      <div className="w-screen absolute block top-0 left-0 bg-secondary text-gray-700 py-5 px-20 m-0 my-0">
+        <div className="w-full h-full">
           <div className="flex items-center justify-between w-full">
             <h1 className="text-4xl font-bold text-text_white">App</h1>
-            <a
-              href=""
-              className="px-6 py-2
-               text-lg px-6 text-text_black bg-accent rounded-full block"
-            >
+            <button className="bg-accent rounded-full text-text_black text-bold text-lg py-2 px-6">
               Logout
-            </a>
+            </button>
           </div>
           <div className="flex items-center justify-between w-full mt-10">
             <h1 className="text-2xl font-bold text-text_white">
@@ -59,30 +101,57 @@ const AdminDashboardPage = () => {
               <img src={arrow} alt="" className="w-4 h-2 rounded-lg" />
             </div>
           </div>
-          <div className="flex items-center border-2 border-text_gray rounded-2xl h-15">
-            <div className="flex items-center w-6/12 gap-3 p-5">
-              <p className="text-sm text-text_gray">01</p>
-              <img
-                src="https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-760x400.png"
-                alt=""
-                className="w-32 rounded-lg"
-              />
-              <p className="text-xl pr-10 text-text_white font-light">
-                Rune raises $100,000 for marketing through NFT butterflies sale
-              </p>
-            </div>
-            <div className="flex justify-between items-center w-6/12 ">
-              <div className="flex items-center gap-4 -mx-10">
-                <img src={avatar} alt="" className="rounded-full h-10 w-10" />
-                <p className="text-xl pr-10 text-text_green font-light">
-                  ninjanft
-                </p>
+          <div className="flex flex-col gap-4">
+            {movieList.map((movie, index) => (
+              <div
+                className="flex items-center border-2 border-text_gray rounded-2xl h-15"
+                key={index}
+              >
+                <div className="flex items-center w-6/12 gap-3 p-5">
+                  <p className="text-sm text-text_gray">{movie.id}</p>
+                  <img
+                    src={movie.photo}
+                    allt={movie.username}
+                    className="w-32 h-16 rounded-lg"
+                  />
+                  <p className="text-xl pr-10 text-text_white font-light">
+                    {movie.title}
+                  </p>
+                </div>
+                <div className="flex justify-between items-center w-6/12 ">
+                  <div className="flex items-center gap-4 -mx-10">
+                    <img
+                      src={avatar}
+                      alt=""
+                      className="rounded-full h-10 w-10"
+                    />
+                    <p className="text-xl pr-10 text-text_green font-light">
+                      {movie.username}
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-4 pr-5">
+                    <p className="text-xl text-text_white font-light">
+                      {movie.like}
+                    </p>
+                    <img src={arrow_up} className="h-5 w-4" />
+                  </div>
+                </div>
               </div>
-              <div className="flex items-center gap-4 pr-5">
-                <p className="text-xl text-text_white font-light">256</p>
-                <img src={arrowup} className="h-5 w-4" />
-              </div>
-            </div>
+            ))}
+          </div>
+          <div className="flex gap-4 mt-10 justify-end">
+            <button
+              className="bg-accent rounded text-text_black text-bold text-lg p-3 px-6"
+              onClick={() => prevPage()}
+            >
+              Previous
+            </button>
+            <button
+              className="bg-accent rounded text-text_black text-bold text-lg p-3 px-6"
+              onClick={() => nextPage()}
+            >
+              Next
+            </button>
           </div>
         </div>
       </div>
